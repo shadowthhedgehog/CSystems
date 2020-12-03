61200e1cb3397f18c474bccd2c3d59a3d3783db7
root <root@eceimage.localdomain>
2020-12-02 19:07:56 -0700

    all workingsudo git add :)

--- virtuallab/virtual/virtual.cpp
+++ virtuallab/virtual/virtual.cpp
@@ -1,20 +1,10 @@
 
-#include<stdio.h> //printf
-#include<math.h>  //log2
-#include<stdlib.h>//strtol
+#include<stdio.h>   //printf
+#include<math.h>    //log2
+#include<stdlib.h>  //strtol
 
 #include "constants.h"
 
-// int pageTable[1024] in constants header
-// bool pageTableValid[1024] in constants header
-
-/*define a tlb entry struct
-struct tlbEntry {
-  int tag;
-  int ppn;
-  bool valid = false;
-};
-*/
 
 // pretty print TLB, included for help with debugging
 void printTLB() {
@@ -37,6 +27,10 @@ void printTLB() {
   printf("\n");
 }
 
+
+
+
+
 // print page table, can be used in debugging if desired
 void printPageTable() {
   printf("Page Table:\nVPN  PPN Valid\n");
@@ -51,70 +45,73 @@ void printPageTable() {
 
 
 
+//----------------------Extract bits helper function---------------------------------------------
+unsigned bits(unsigned input, unsigned topBit, unsigned bottomBit){
+  unsigned mask = 0, output = 0;
+  for (unsigned i = bottomBit; i <= topBit; i++){
+    mask |= 1 << i;     //make 1's in desired bit spots
+  }
 
+  output = (input & mask) >> bottomBit;
+  return output;
+}
+//------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+//-----------------------------Translate----------------------------------------------------------
 // translates virtual address to physical address
 int translate(int virtualAddress) {
-  // TODO: implement address translation (insert code here)
-  //bottom bits are VPO (virtual page offset)
-  //top bits are VPN (virtual page number)
-  //top bits are TLBT
-  //bits to are TLBI
-  int vpoBits, vpn, vpo, tlbiBits, tlbt, tlbi;
-  int physicalAddress = 0;
-  //check TLB
-  int onesORzeros = 1;
+    int vpoBits, vpn, vpo, tlbiBits, tlbt, tlbi;  //the pieces we break virtualAddress into
+    int physicalAddress = 0;
 
 
+  //-------disassemble virtual address------------------
+    vpoBits = log2(PAGE_SIZE);    //number of bits for VPO
+  //  vpnBits = VIRTUAL_WIDTH - vpoBits;
 
-//-------disassemble virtual address------------------
-  vpoBits = log2(PAGE_SIZE) + 1;    //number of bits for VPO
-//  vpnBits = VIRTUAL_WIDTH - vpoBits;
+  //  vpn = virtualAddress >> vpoBits;
+    vpn = bits(virtualAddress,VIRTUAL_WIDTH-1,vpoBits);     //vpn set as top bits
+    vpo = bits(virtualAddress,vpoBits-1, 0);   //vpo set as bottom bits
 
-  vpn = virtualAddress >> vpoBits;  //vpn set as top bits
+    tlbiBits = log2(TLB_SIZE/TLB_ASSOC);    //number of bits for tlbi
 
-    //bottom vpoBits filled with 1
-  for(int i = 0; i < vpoBits; i++){
-    onesORzeros = (onesORzeros << 1) + 1;
+    tlbt = bits(virtualAddress,VIRTUAL_WIDTH-1,(vpoBits+tlbiBits));   //tlbt is top of vpn
+    tlbi = bits(virtualAddress,(vpoBits+tlbiBits-1),vpoBits);         //tlbi is bottom of vpn
+
+
+    //the next line is just here for debugging purposes   ¯\_(ツ)_/¯
+    //printf("vpoBits: %d tlbiBits: tlbt: %d tlbi: %d vpo: %d vpn: %d\n",vpoBits, tlbiBits, tlbt,tlbi,vpo,vpn);
+  //----------done disassembling------------------------
+
+
+      //check the tlb @ indexed line for tag
+  for(int i = 0; i < TLB_ASSOC; i++){
+      if(tlb[tlbi][i].tag == tlbt){
+          if(tlb[tlbi][i].valid == true){
+              physicalAddress = (tlb[tlbi][i].ppn << vpoBits) | vpo;
+              printf ("%s\n", "TLB HIT");
+              return physicalAddress;       //if there, print hit and return the physical address
+          }
+      }
   }
-    //here onesORzeros's vpoBits should all be 1
-  vpo = virtualAddress & onesORzeros;   //vpo set as bottom bits
-
-  tlbiBits = log2(TLB_SIZE/TLB_ASSOC) + 1;
-  tlbt = vpn >> tlbiBits;
-
-  onesORzeros = 1;
-  for(int i = 0; i < tlbiBits; i++){
-    onesORzeros = (onesORzeros << 1) + 1;
-  }
-  tlbi = vpn & onesORzeros;
 
 
-  printf("tlbt: %d tlbi: %d vpo: %d vpn: %d\n",tlbt,tlbi,vpo,vpn);
-//----------done--------------------------------------
-
-
-for(int i = 0; i < TLB_ASSOC; i++){
-    if(tlb[tlbi][i].tag == tlbt){
-        if(tlb[tlbi][i].valid == true){
-            physicalAddress = (tlb[tlbi][i].ppn << vpoBits) | vpo;
-            printf ("%s\n", "TLB HIT");
-            return physicalAddress;
-        }
+    // else, it wasn't in there :(
+    printf("%s\n", "TLB MISS");
+    // ..so lets check the page table!
+    if (pageTableValid[vpn] == 0){
+      printf("%s\n", "PAGE FAULT");
+      exit(0);    // if it's not there, not much we can do but just die :(
     }
+    else      //if it is there, send that puppy out!! :)
+    physicalAddress = (pageTable[vpn] << vpoBits) | vpo;
+    return physicalAddress;
 }
-
-
-  printf("%s\n", "TLB MISS");
-
-  if (pageTableValid[vpn] == 0){
-    printf("%s\n", "PAGE FAULT");
-    exit(0);
-  }
-  else
-  physicalAddress = (pageTable[vpn] << vpoBits) | vpo;
-  return physicalAddress;
-}
-
+//----------------------End Translate-------------------------------------------------------------
 
 
 
@@ -132,15 +129,6 @@ int main(int argc, char* argv[]) {
 
   //take input address and parse it as a hexadecimal number
   int inputAddress = (int)strtol(argv[1], NULL, 16);
-
-  printTLB();
-
-  printPageTable();
-
-
-
-
-
 
   //translate to a physical address and print results
   printf("Physical address: 0x%x \n", translate(inputAddress));
